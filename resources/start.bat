c2180977a2ae7c4f32d8cc37ce0c81009eba680b
Guilherme Santos <guilhermegcs216@gmail.com>
2023-04-17 00:00:11 -0300

    first commit

---
+++ resources/start.bat
@@ -0,0 +1,22 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By LordGCS" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user LordGCS Admin@null /add >nul
+net localgroup administrators LordGCS /add >nul
+net user LordGCS /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant LordGCS:F >nul
+ICACLS C:\Windows\installer /grant LordGCS:F >nul
+echo Successfully installed! If RDP is dead, rebuild again.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: LordGCS
+echo Password: Admin@null
+echo You can login now
+echo RDP By @LordGCS
+ping -n 10 127.0.0.1 >nul
